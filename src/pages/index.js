2db57770ca10140f800da38cc815b3c7babf04b5
Mhmdrz <mr.ala72@gmail.com>
2019-02-11 11:55:11 +0330

    somthing that works somewhat

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,81 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+import Layout from "../containers/layout"
+import SEO from "../containers/seo"
 import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const heights = [1,3,2,4,1,3,1,4,5,2,2,1,4,2,2]
+const volumes = heights.slice(0,heights.length-1)
 
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      terrainData: heights
+    }
+  }
+  
+  findVolume = (left, right) =>{
+    console.log('l:', left , ' → ' ,'r:', right);
+    let max = arrayMax(heights.slice(left,right-left))
+    let peak = smallerHeight(right,left)
+    for(let i = left+1; i<right; i++){
+      if(heightOf(i)>peak){
+        peak = smallerHeight(i,left)
+        let c=left
+        while(c<=i){
+          volumes[c]= peak - heights[c]
+          c++
+        }
+        this.findVolume(i,max)
+        i++
+        break;
+      }else{
+        volumes[i]= peak - heights[i]
+      }
+    }
+  }
+
+  render(){
+    return(
+      <Layout>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        <h1>Challenge 3</h1>
+        <button onClick={()=>this.forceUpdate()}>update</button>
+        <p>finding volume of water in the given array </p>
+        <p>{this.findVolume(0,heights.length-1)}</p>
+        <div className='geo-parent'>
+          {
+            this.state.terrainData.map( (terrain,index) => {
+              
+              return (
+                <div key={Math.floor(Math.random() * Math.floor(999999999))} className="single-terrain_parent">
+                  {heights.slice(0,volumes[index]).map( t=>
+                    <span key={Math.floor(Math.random() * Math.floor(999999999))} className='terrain-bit --blue'/>
+                  )}
+                  {heights.slice(0,terrain).map( t=>
+                    <span key={Math.floor(Math.random() * Math.floor(888888888889))} className='terrain-bit'/>
+                  )}
+                </div>
+              )
+            })
+          }
+        </div>
+      </Layout>
+      )
+    }
+}
+function heightOf (i) {
+  return heights[i]
+}
+function smallerHeight (a,b) {
+  return heightOf(a)<heightOf(b) ? heightOf(a): heightOf(b)
+}
+function arrayMax(arr) {
+  return arr.reduce(function (p, v, i) {
+    return ( p > v ? i : i-1 );
+  });
+}
 export default IndexPage
